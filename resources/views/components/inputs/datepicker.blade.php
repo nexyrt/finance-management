cc1eb3533d643a28bf2a0cdb69ce771c2493b3ce
Rizky Hamdani <rizkyyhamdani21@gmail.com>
2025-06-20 14:02:03 +0800

    Refactor datepicker component for improved styling and validation handling

--- resources/views/components/inputs/datepicker.blade.php
+++ resources/views/components/inputs/datepicker.blade.php
@@ -8,7 +8,32 @@
     'enableTime' => false,
     'minDate' => null,
     'maxDate' => null,
+    'variant' => 'outline',
+    'invalid' => null,
 ])
+
+@php
+$invalid ??= ($name && $errors->has($name));
+
+$classes = Flux::classes()
+    ->add('w-fit border rounded-lg block disabled:shadow-none dark:shadow-none')
+    ->add('appearance-none')
+    ->add('text-base sm:text-sm py-2 h-10 leading-[1.375rem] px-3')
+    ->add(match ($variant) { // Background...
+        'outline' => 'bg-white dark:bg-white/10 dark:disabled:bg-white/[7%]',
+        'filled'  => 'bg-zinc-800/5 dark:bg-white/10 dark:disabled:bg-white/[7%]',
+    })
+    ->add(match ($variant) { // Text color
+        'outline' => 'text-zinc-700 disabled:text-zinc-500 placeholder-zinc-400 disabled:placeholder-zinc-400/70 dark:text-zinc-300 dark:disabled:text-zinc-400 dark:placeholder-zinc-400 dark:disabled:placeholder-zinc-500',
+        'filled'  => 'text-zinc-700 placeholder-zinc-500 disabled:placeholder-zinc-400 dark:text-zinc-200 dark:placeholder-white/60 dark:disabled:placeholder-white/40',
+    })
+    ->add(match ($variant) { // Border...
+        'outline' => $invalid ? 'border-red-500' : 'shadow-xs border-zinc-200 border-b-zinc-300/80 disabled:border-b-zinc-200 dark:border-white/10 dark:disabled:border-white/5',
+        'filled'  => $invalid ? 'border-red-500' : 'border-0',
+    })
+    ->add('focus:ring-2 focus:ring-blue-500 focus:outline-none')
+    ->add($class);
+@endphp
 
 <input 
     x-data 
@@ -33,6 +58,7 @@
         }
     })" 
     type="text" 
-    {{ $attributes->merge(['class' => 'bg-zinc-700 rounded-lg px-3 py-1.5 text-zinc-700 dark:text-zinc-300 placeholder-gray-400 border-[1px] border-zinc-400 focus:ring-2 focus:ring-blue-500 focus:outline-none ' . $class]) }}
+    {{ $attributes->except('class')->class($classes) }}
     placeholder="{{ $placeholder }}"
+    @if ($invalid) aria-invalid="true" data-invalid @endif
 >
