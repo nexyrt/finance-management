6e013517146a1ecf3983f826ff4d2da567fad227
Rizky Hamdani <rizkyyhamdani21@gmail.com>
2025-07-11 09:59:11 +0800

    Remove Cleave.js and Intl.NumberFormat input handling from TestingPage

--- app/Livewire/TestingPage.php
+++ app/Livewire/TestingPage.php
@@ -6,17 +6,7 @@ use Livewire\Component;
 
 class TestingPage extends Component
 {
-    public $cleaveAmount = 150000;
-    public $intlAmount = 150000;
-
-    public function submit()
-    {
-        // Cek hasil input
-        dd([
-            'cleave' => $this->cleaveAmount,
-            'intl' => $this->intlAmount,
-        ]);
-    }
+    
 
     public function render()
     {
