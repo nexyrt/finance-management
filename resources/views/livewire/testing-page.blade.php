7fcef1318a1cf33a00b9da68a86fdcf2e439374b
Rizky Hamdani <rizkyyhamdani21@gmail.com>
2025-07-20 07:52:30 +0800

    refactor: Remove editor section from TestingPage component

--- resources/views/livewire/testing-page.blade.php
+++ resources/views/livewire/testing-page.blade.php
@@ -1,7 +1,3 @@
 <div class="max-w-7xl mx-auto p-4">
-    <div id="editor">
-        <p>Hello World!</p>
-        <p>Some initial <strong>bold</strong> text</p>
-        <p><br /></p>
-    </div>
+    
 </div>
