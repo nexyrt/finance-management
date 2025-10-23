3b9dfb81647f0a13fc0a37b5e98615cf5261f2f9
Rizky Hamdani <rizkyyhamdani21@gmail.com>
2025-10-23 14:05:38 +0800

    spatie/permission installation

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,13 +6,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
-
+    use HasFactory, Notifiable,HasRoles;
     /**
      * The attributes that are mass assignable.
      *
